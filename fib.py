f3e806e8885f75699f988c882912eba00da2c8c0
Johannes Bechberger <johannes.bechberger@sap.com>
2023-09-19 15:17:20 +0200

    Fix scripts

---
+++ fib.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def fib(n: int) -> int:
+    if n <= 1:
+        f = n
+    else:
+        f1 = fib(n - 1)
+        f2 = fib(n - 2)
+        f = f1 + f2
+    return f
+
+
+if __name__ == '__main__':
+    n = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+    print(fib(n))
+
--- misc/dbg_base.py
+++ misc/dbg_base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 from pathlib import Path
 
 
@@ -13,6 +14,7 @@ class Dbg:
     def run(self, file: Path):
         # see https://realpython.com/python-exec/#using-python-for-configuration-files
         compiled = compile(file.read_text(), filename=file.name, mode='exec')
+        sys.argv.pop(0)
         # set stuff here
         try:
             exec(compiled, globals())
@@ -23,10 +25,11 @@ class Dbg:
 if __name__ == '__main__':
     argparser = argparse.ArgumentParser()
     argparser.add_argument("file", help="file to debug")
+    argparser.add_argument("args", nargs="*", help="arguments to pass to file")
     args = argparser.parse_args()
     dbg = Dbg()
     try:
-        dbg.run(args.file)
+        dbg.run(Path(args.file))
     except DbgQuit:
         pass
     except KeyboardInterrupt:
